bfca1165640eabf755ce91f0cc63543202d712fb
nanidam <nani.dam@medieinstitutet.se>
2023-10-30 21:47:03 +0100

    :bug: fix footer + header bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/* <Header></Header> */}
-      <body className={inter.className}>{children}</body>
-      {/* <Footer /> */}
-    </html>
+    <>
+      <Header />
+      <div className={inter.className}>{children}</div>
+      <Footer />
+    </>
   );
 }
