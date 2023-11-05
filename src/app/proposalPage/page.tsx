ad25dd69f55833e8725fbe7f03f714a0afe50913
nanidam <nani.dam@medieinstitutet.se>
2023-11-05 15:47:24 +0100

    ➕ added proposal page

---
+++ src/app/proposalPage/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import "./style.scss";
+
+export default function ProposalPage() {
+  return (
+    <>
+      <section className="wrapper">
+        <h1>Frudd proposed</h1>
+        <p>
+          On august 3rd 2023, <br /> Frudd got down on one knee in the restaurant Spagio
+          infront of the Balliagio fountain and asked Nani to marry him... and she said yes!
+        </p>
+      </section>
+    </>
+  );
+}
