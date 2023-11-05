c8fb8482dcffee039fa408eb94b14fcba1a3bbdc
nanidam <nani.dam@medieinstitutet.se>
2023-11-05 21:56:40 +0100

    WIP

--- src/app/dress-code/page.tsx
+++ src/app/dress-code/page.tsx
@@ -1,10 +1,24 @@
 // import Image from "next/image";
+import Link from "next/link";
+import Image from "next/image";
+
 import "./style.scss";
 
 export default function DressCode() {
   return (
-    <>
-      <h1>dress code</h1>
-    </>
+    <section className="wrapper">
+      <article className="dress-code">
+        <h1>Wedding dress code</h1>
+        <h3>Dressy casual / semi-formal attire</h3>
+        <div>
+          <p>Here are some reading tips:</p>
+          <Link href="/">Link 1</Link> <br />
+          <Link href="/">Link 1</Link>
+        </div>
+      </article>
+      <article className="hide-mobile">
+        <Image src="/attire.png" alt="Dress code" width={400} height={600} />
+      </article>
+    </section>
   );
 }
