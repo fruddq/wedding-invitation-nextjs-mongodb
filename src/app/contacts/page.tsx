4d0f5a977c56657707d1ce59a4a514aa01f7de70
nanidam <nani.dam@medieinstitutet.se>
2023-11-05 22:56:33 +0100

    WIP

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+import Link from "next/link";
+import Image from "next/image";
+
+import "./style.scss";
+
+export default function Contacts() {
+  return (
+    <>
+      <h1>contacts</h1>
+    </>
+  );
+}
