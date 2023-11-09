1a1deb1d3742378a78d87c6bdc95b25939d0b9e6
nanidam <nani.dam@medieinstitutet.se>
2023-11-09 22:05:07 +0100

    ➕ added contact page for mobile + desktop layout

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -8,41 +8,71 @@ export default function Contacts() {
   return (
     <>
       <h1>contacts</h1>
-      <section>
-        <article className="toastmadame">
-          <Image src="/toastmadame.png" alt="Toastmasdame" width={100} height={100} />
-
-          <p>
-            Toastmadame <span>Susanna Charboti</span>
-          </p>
-          <p>
-            Phone <span>+46 73 966 68 10</span>
-          </p>
-          <p>
-            E-mail <span>Nfz2A@example.com</span>
-          </p>
-        </article>
-        <article className="toastmaster">
-          <Image src="/toastmaster.png" alt="Toastmaster" width={100} height={100} />
-          <p>
-            Toastmaster <span>David Tran</span>
-          </p>
-          <p>
-            Phone <span>+46 73 755 44 04</span>
-          </p>
-          <p>
-            E-mail <span>Nfz2A@example.com</span>
-          </p>
-        </article>
+      <p className="info">
+        If you have any questions related to the wedding please contact our toastmadame or
+        toastmaster.{" "}
+      </p>
+      <section className="wrapper">
+        <div className="bg">
+          <article className="toastmadame">
+            <Image
+              className="pic"
+              src="/toastmadame.png"
+              alt="Toastmasdame"
+              width={150}
+              height={150}
+            />
+            <div>
+              <h3>Toastmadame</h3>
+              <p className="name">
+                Susanna <br />
+                Charboti
+              </p>
+            </div>
+            <div>
+              <h4>Phone</h4>
+              <p className="phone">+46 73 966 68 10</p>
+            </div>
+            <div>
+              <h4>E-mail</h4>
+              <p className="email">Nfz2A@example.com</p>
+            </div>
+          </article>
+          <hr />
+          <article className="toastmaster">
+            <Image
+              className="pic"
+              src="/toastmaster.png"
+              alt="Toastmaster"
+              width={150}
+              height={150}
+            />
+            <div>
+              <h3>Toastmaster</h3>
+              <p className="name">
+                David <br /> Tran
+              </p>
+            </div>
+            <div>
+              <h4>Phone</h4>
+              <p className="phone">+46 73 755 44 04</p>
+            </div>
+            <div>
+              <h4>E-mail</h4>
+              <p className="email">Nfz2A@example.com</p>
+            </div>
+          </article>
+        </div>
       </section>
-      <section>
+      {/* <section>
         <Image
-          src="/champagne-and-bottle.png"
+          className="extra-pic"
+          src="/balloons.png"
           alt="Champagne glass with popped champagne bottle"
-          width={500}
-          height={650}
+          width={750}
+          height={550}
         />
-      </section>
+      </section> */}
     </>
   );
 }
