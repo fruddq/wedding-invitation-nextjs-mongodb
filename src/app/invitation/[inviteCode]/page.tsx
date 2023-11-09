d68fe411cceaa758041c2a5c27053daa793ebbcf
Frudd <giadi735@gmail.com>
2023-11-09 22:46:53 +0100

    Fixed guest credentials :hammer:

--- src/app/invitation/[inviteCode]/page.tsx
+++ src/app/invitation/[inviteCode]/page.tsx
@@ -2,13 +2,36 @@
 import { useState } from "react"
 import LoginForm from "@/components/loginForm"
 
+type AdditionalGuest = {
+  firstName: string
+  lastName: string
+  comments: string | undefined
+  diet: string | undefined
+}
+export interface GuestInterface {
+  id: string
+  firstName: string
+  lastName: string
+  email: string | null
+  phoneNumber: number | null
+  diet: string | null
+  comments: string | null
+  eventId: string
+  attending: boolean
+  hasResponded: boolean
+  inviteSent: boolean
+}
 export default function Invitation() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [guest, setGuest] = useState<GuestInterface | null>(null)
+  console.log(guest)
 
   return (
     <>
-      <LoginForm setIsLoggedIn={setIsLoggedIn} />
-      {isLoggedIn && <p>You are logged in!</p>}
+      {guest ? (
+        <p>You are logged in as {guest.firstName}</p>
+      ) : (
+        <LoginForm setGuest={setGuest} />
+      )}
     </>
   )
 }
