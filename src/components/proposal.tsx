859da7f048ebb9cb74ea7eddeb8ce16bd29854c2
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 16:50:34 +0100

    WIP

--- src/components/proposal.tsx
+++ src/components/proposal.tsx
@@ -28,8 +28,8 @@ export default function Proposal() {
           className="hide-mobile"
           src="/proposal-people 1.png"
           alt="Champagne glass with popped champagne bottle"
-          width={400}
-          height={400}
+          width={500}
+          height={500}
         />
       </section>
     </div>
