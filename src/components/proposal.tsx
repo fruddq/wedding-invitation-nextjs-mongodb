8f3e69bb00c9c93e44eed1b0309e99e2688bd804
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 14:26:32 +0100

    WIP

--- src/components/proposal.tsx
+++ src/components/proposal.tsx
@@ -7,18 +7,20 @@ export default function Proposal() {
       <section className="wrapper-proposal-left">
         <div className="info">
           <h1 className="proposal-title">Frudd proposed</h1>
-          <p className="proposal-text top-border">
-            On August 3rd, 2023, at the enchanting Spagio restaurant in Las Vegas, against
-            the backdrop of the majestic Bellagio fountain.
-          </p>
-          <p className="proposal-text">
-            Before dessert arrived, in a moment filled with anticipation, Frudd knelt down
-            on one knee and asked Nani the most important question of his life — 'Will you
-            marry me?'
-          </p>
-          <p className="proposal-text bottom-border">
-            Stunned and choked with emotion, she responded with a heartfelt 'Yes!'
-          </p>
+          <div className="proposal-wrapper">
+            <p className="proposal-text top-border">
+              On August 3rd, 2023, at the enchanting Spagio restaurant in Las Vegas, against
+              the backdrop of the majestic Bellagio fountain.
+            </p>
+            <p className="proposal-text">
+              Before dessert arrived, in a moment filled with anticipation, Frudd knelt down
+              on one knee and asked Nani the most important question of his life — 'Will you
+              marry me?'
+            </p>
+            <p className="proposal-text bottom-border">
+              Stunned and choked with emotion, she responded with a heartfelt 'Yes!'
+            </p>
+          </div>
         </div>
       </section>
       <section className="wrapper-proposal-right">
--- src/components/theBigDay.tsx
+++ src/components/theBigDay.tsx
@@ -1,36 +1,38 @@
-/* eslint-disable react/no-unescaped-entities */
-import { PreviousAndNextBtn } from "./previousAndNextBtn"
-import "./styles/theBigDay.scss"
+import "./styles/theBigDay.scss";
+import Image from "next/image";
 
 export default function TheBigDay() {
   return (
-    <section className="container">
-      <div className="big-day-wrapper">
-        <h1>The big day!</h1>
-        <article className="evening-info">
-          <p>
-            Our evening will commence with a selection of hors d'oeuvres,
-            followed by a main dish and a mouthwatering cake for dessert. Each
-            course will be thoughtfully paired with fine wines. Rest assured,
-            our vegetarian guests will be catered to with equally delectable
-            alternatives.
-          </p>
-          <p>
-            The enchanting atmosphere will be accentuated by the soothing
-            melodies of music, providing a harmonious backdrop for shared
-            laughter and cherished moments. <br />
-          </p>
-          <p>
-            Please understand that this gathering is exclusively for adults, and
-            we kindly request that children not be in attendance.
-          </p>
-          <p>
-            We eagerly anticipate the pleasure of your company, and we are
-            committed to ensuring that this special evening becomes an indelible
-            memory for us all!
-          </p>
-        </article>
-      </div>
+    <section className="big-day-container">
+      <h1>The big day!</h1>
+      <article className="evening-info">
+        <p className="evenining-text">
+          Our evening will commence with a selection of hors d'oeuvres, followed by a main
+          dish and a mouthwatering cake for dessert. Each course will be thoughtfully paired
+          with fine wines. Rest assured, our vegetarian guests will be catered to with
+          equally delectable alternatives.
+        </p>
+        <p className="evenining-text">
+          The enchanting atmosphere will be accentuated by the soothing melodies of music,
+          providing a harmonious backdrop for shared laughter and cherished moments. <br />
+        </p>
+        <p className="evenining-text">
+          Please understand that this gathering is exclusively for adults, and we kindly
+          request that children not be in attendance.
+        </p>
+        <p className="evenining-text">
+          We eagerly anticipate the pleasure of your company, and we are committed to
+          ensuring that this special evening becomes an indelible memory for us all!
+        </p>
+        <div className="plate-img">
+          <Image
+            src="/plate-less-opacity.png"
+            alt="The big day"
+            width={300}
+            height={200}
+          ></Image>
+        </div>
+      </article>
     </section>
-  )
+  );
 }
