2887fc8295a0c3572d8b2dcb0dee4dabe56fd19b
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 21:08:30 +0100

    added classnames

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,75 +1,78 @@
-import { GuestInterface } from "@/app/invitation/[inviteCode]/page"
-import checkGuestCredentials from "@/utils/checkGuestCredentials"
+import { GuestInterface } from "@/app/invitation/[inviteCode]/page";
+import checkGuestCredentials from "@/utils/checkGuestCredentials";
+import "./styles/loginForm.scss";
 
 interface LoginFormProps {
-  setGuest: (guest: GuestInterface | null) => void
+  setGuest: (guest: GuestInterface | null) => void;
 }
 export const LoginForm: React.FC<LoginFormProps> = ({ setGuest }) => {
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const formData = new FormData(e.currentTarget)
-    const firstName = formData.get("firstName") as string
-    const lastName = formData.get("lastName") as string
-    const password = formData.get("password") as string // Add this line
+    const formData = new FormData(e.currentTarget);
+    const firstName = formData.get("firstName") as string;
+    const lastName = formData.get("lastName") as string;
+    const password = formData.get("password") as string; // Add this line
 
-    const guest = await checkGuestCredentials({ firstName, lastName, password })
+    const guest = await checkGuestCredentials({ firstName, lastName, password });
 
     if (guest) {
-      localStorage.setItem("guest", JSON.stringify(guest))
-      setGuest(guest)
+      localStorage.setItem("guest", JSON.stringify(guest));
+      setGuest(guest);
     }
-  }
+  };
+
   return (
-    <section className="container">
-      <h1>
-        Welcome to <br /> <span>Nani & Fruddis</span> <br />
+    <section className="login-container">
+      <h1 className="login-title">
+        Welcome to <br /> <span className="login-span">Nani & Fruddi's</span> <br />
         wedding reception
       </h1>
-      <article className="info-text">
-        <p>
-          Please enter your first name, last name, and password below for more
-          wedding details.
+      <article className="login-text-wrapper">
+        <p className="login-text">
+          Please enter your first name, last name, and password below for more wedding
+          details.
         </p>
       </article>
-      <article className="form-container">
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="firstName">First Name:</label>
-            <input
-              type="text"
-              name="firstName"
-              placeholder="Your first name"
-              defaultValue="Jens"
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name:</label>
-            <input
-              type="text"
-              name="lastName"
-              placeholder="Your last name"
-              defaultValue="Persson"
-            />
-          </div>
-          <div>
-            {" "}
-            {/* Add this div */}
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Your password"
-              defaultValue="NaniFruddIDo2024!"
-            />
-          </div>
-          <button className="login-btn" type="submit">
-            Login
-          </button>
-        </form>
-      </article>
+      <form className="form-container" onSubmit={handleLogin}>
+        <div className="form-wrapper">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            className="login-input"
+            type="text"
+            name="firstName"
+            placeholder="Your first name"
+            defaultValue="Jens"
+          />
+        </div>
+        <div className="form-wrapper">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            className="login-input"
+            type="text"
+            name="lastName"
+            placeholder="Your last name"
+            defaultValue="Persson"
+          />
+        </div>
+        <div className="form-wrapper">
+          {" "}
+          {/* Add this div */}
+          <label htmlFor="password">Password:</label>
+          <input
+            className="login-input"
+            type="password"
+            name="password"
+            placeholder="Your password"
+            defaultValue="NaniFruddIDo2024!"
+          />
+        </div>
+        <button className="login-btn" type="submit">
+          Login
+        </button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
