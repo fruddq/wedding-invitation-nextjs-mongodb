c9cb53f6002340eb556b61b14e30cb4af1b0ca3a
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 14:49:21 +0100

    fixing layout mobile

--- src/components/welcomePage.tsx
+++ src/components/welcomePage.tsx
@@ -1,15 +1,15 @@
-import { PreviousAndNextBtn } from "./previousAndNextBtn"
-import "./styles/welcomePage.scss"
-import { GuestInterface } from "@/app/invitation/[inviteCode]/page"
+import { PreviousAndNextBtn } from "./previousAndNextBtn";
+import "./styles/welcomePage.scss";
+import { GuestInterface } from "@/app/invitation/[inviteCode]/page";
 
 export default function WelcomePage({ guest }: { guest: GuestInterface }) {
   return (
     <>
       <section className="wrapper-invitation">
-        <h1>Welcome to our wedding</h1>
-        <h4>2024</h4>
-        <p>A special invitation to:</p>
-        <span>
+        <h1 className="welcome-title">Welcome to our wedding</h1>
+        <h4 className="sub-title">2024</h4>
+        <p className="welcome-text">A special invitation to:</p>
+        <span className="welcome-span">
           {guest.additionalGuests && guest.additionalGuests.length > 0 && (
             <>
               {guest.firstName} {guest.lastName} <br />
@@ -26,5 +26,5 @@ export default function WelcomePage({ guest }: { guest: GuestInterface }) {
         </span>
       </section>
     </>
-  )
+  );
 }
