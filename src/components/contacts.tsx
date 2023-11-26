c5c8cc4809fb1a7ccf8961a9bef73c2c80616689
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 17:29:44 +0100

    added classnames

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -1,15 +1,15 @@
 // import Image from "next/image";
-import Image from "next/image"
+import Image from "next/image";
 
-import "./styles/contacts.scss"
+import "./styles/contacts.scss";
 
 export default function Contacts() {
   return (
     <div className="contacts">
-      <h1>Contacts</h1>
+      <h1 className="contatcs-title">Contacts</h1>
       <p className="info">
-        If you have any questions related to the wedding please contact our
-        toastmadame or toastmaster.{" "}
+        If you have any questions related to the wedding please contact our toastmadame or
+        toastmaster.{" "}
       </p>
       <section className="wrapper-contacts">
         <div className="bg">
@@ -22,18 +22,18 @@ export default function Contacts() {
               height={150}
             />
             <div className="wrapper-toast">
-              <h3>Toastmadame</h3>
+              <h3 className="contacts-subtitle">Toastmadame</h3>
               <p className="name">
                 Susanna <br />
                 Charboti
               </p>
             </div>
             <div className="wrapper-toast">
-              <h4>Phone</h4>
+              <h4 className="contacts-subtitle">Phone</h4>
               <p className="phone">+46 73 966 68 10</p>
             </div>
             <div className="wrapper-toast">
-              <h4>E-mail</h4>
+              <h4 className="contacts-subtitle">E-mail</h4>
               <p className="email">Nfz2A@example.com</p>
             </div>
           </article>
@@ -47,22 +47,22 @@ export default function Contacts() {
               height={150}
             />
             <div className="wrapper-toast">
-              <h3>Toastmaster</h3>
+              <h3 className="contacts-subtitle">Toastmaster</h3>
               <p className="name">
                 David <br /> Tran
               </p>
             </div>
             <div className="wrapper-toast">
-              <h4>Phone</h4>
+              <h4 className="contacts-subtitle">Phone</h4>
               <p className="phone">+46 73 755 44 04</p>
             </div>
             <div className="wrapper-toast">
-              <h4>E-mail</h4>
+              <h4 className="contacts-subtitle">E-mail</h4>
               <p className="email">Nfz2A@example.com</p>
             </div>
           </article>
         </div>
       </section>
     </div>
-  )
+  );
 }
