bdcb6c3ab2b4fa9602604b0e5b392f542ab8766f
nanidam <nani.dam@medieinstitutet.se>
2023-11-26 17:32:21 +0100

    added classnames

--- src/components/outro.tsx
+++ src/components/outro.tsx
@@ -1,18 +1,17 @@
 import Image from "next/image";
 import "./styles/outro.scss";
-import { pagination } from "./pagination";
 
 export default function Outro() {
   return (
     <>
       <section className="wrapper-outro">
         <article className="text-container">
-          <h1>See you on our big day!</h1>
-          <p>Your presence will mean so much to us</p>
+          <h1 className="outro-title">See you on our big day!</h1>
+          <p className="outro-text">Your presence will mean so much to us</p>
         </article>
         <article>
           <Image
-            className="pic"
+            className="outro-pic"
             src="/car.png"
             alt="The back of a car driving away"
             width={400}
