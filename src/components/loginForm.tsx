2e6b7fb642d3f2947e2308789a3ddfefd1410619
Frudd <giadi735@gmail.com>
2023-11-28 18:14:26 +0100

    Fixed loader & background image in RSVP :hammer:

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,47 +1,48 @@
 /* eslint-disable react/no-unescaped-entities */
-import { GuestInterface } from "@/app/invitation/[inviteCode]/page";
-import checkGuestCredentials from "@/utils/checkGuestCredentials";
-import "./styles/loginForm.scss";
-import Image from "next/image";
-import { useState } from "react";
-import { PacmanLoader } from "react-spinners";
+import { GuestInterface } from "@/app/invitation/[inviteCode]/page"
+import checkGuestCredentials from "@/utils/checkGuestCredentials"
+import "./styles/loginForm.scss"
+import Image from "next/image"
+import { useState } from "react"
+import { PacmanLoader } from "react-spinners"
 
 interface LoginFormProps {
-  setGuest: (guest: GuestInterface | null) => void;
+  setGuest: (guest: GuestInterface | null) => void
 }
 export const LoginForm: React.FC<LoginFormProps> = ({ setGuest }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const formData = new FormData(e.currentTarget);
-    const firstName = formData.get("firstName") as string;
-    const lastName = formData.get("lastName") as string;
-    const password = formData.get("password") as string; // Add this line
+    const formData = new FormData(e.currentTarget)
+    const firstName = formData.get("firstName") as string
+    const lastName = formData.get("lastName") as string
+    const password = formData.get("password") as string // Add this line
 
-    setLoading(true);
-    const guest = await checkGuestCredentials({ firstName, lastName, password });
+    setLoading(true)
+    const guest = await checkGuestCredentials({ firstName, lastName, password })
 
     if (guest) {
-      localStorage.setItem("guest", JSON.stringify(guest));
-      setGuest(guest);
+      localStorage.setItem("guest", JSON.stringify(guest))
+      setGuest(guest)
     }
-    console.log(loading);
-    setLoading(false);
-  };
+    console.log(loading)
+    setLoading(false)
+  }
 
   return (
     <section className="login-container">
       {!loading ? (
         <div className="login-wrapper-left">
           <h1 className="login-title">
-            Welcome to <br /> <span className="login-span">Nani & Fruddi's</span> <br />
+            Welcome to <br />
+            <span className="login-span">Nani & Fruddi's</span> <br />
             wedding reception
           </h1>
           <article className="login-text-wrapper">
             <p className="login-text">
-              Please enter your first name, last name, and password below for more wedding
-              details.
+              Please enter your first name, last name, and password below for
+              more wedding details.
             </p>
           </article>
           <form className="form-container" onSubmit={handleLogin}>
@@ -66,8 +67,6 @@ export const LoginForm: React.FC<LoginFormProps> = ({ setGuest }) => {
               />
             </div>
             <div className="form-wrapper">
-              {" "}
-              {/* Add this div */}
               <label htmlFor="password">Password:</label>
               <input
                 className="login-input"
@@ -83,22 +82,29 @@ export const LoginForm: React.FC<LoginFormProps> = ({ setGuest }) => {
           </form>
         </div>
       ) : (
-        <PacmanLoader
-          color="orange"
-          loading={loading}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
+        <div className="loader-container">
+          <PacmanLoader
+            color="orange"
+            loading={loading}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
       )}
 
       <div className="login-wrapper-right">
         <article className="login-img">
-          <Image src="/champagne-and-bottle.png" alt="logo" width={600} height={900} />
+          <Image
+            src="/champagne-and-bottle.png"
+            alt="logo"
+            width={600}
+            height={900}
+          />
         </article>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default LoginForm
