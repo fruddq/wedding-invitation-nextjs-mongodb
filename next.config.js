df1a77c5cfb1e8d24f7522f604efaebfb926c08b
Frudd <giadi735@gmail.com>
2023-11-28 21:01:47 +0100

    Test next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         serverActions:true
     },
     basePath: '/github-pages',
-    reactStrictMode: true,
+    reactStrictMode: false,
     output: 'export',
     trailingSlash: true,
 
