7023d1cbeab584aba9e49343c5f3dc996330dcea
Frudd <giadi735@gmail.com>
2023-11-28 21:12:58 +0100

    Nextconfig test

--- next.config.js
+++ next.config.js
@@ -1,25 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Remove the invalid 'images' property from the 'experimental' object
     experimental: {
       serverActions: true,
     },
+  
+    // Set the base path for the application
     basePath: '/github-pages',
-    reactStrictMode: false,
+  
+    // Enable strict mode for React components
+    reactStrictMode: true,
+  
+    // Set the output type to 'export'
     output: 'export',
+  
+    // Enable trailing slashes in URLs
     trailingSlash: true,
   
-    exportPathMap: async function () {
+    // Use 'generateStaticParams' instead of 'exportPathMap' for dynamic routes
+    async rewrites() {
       const paths = await generateStaticParams();
-      const pathMap = {};
+      const rewrites = [];
   
       paths.forEach((path) => {
-        pathMap[`/admin/event/${path.params.eventName}`] = {
-          page: '/admin/event/[eventName]',
+        rewrites.push({
+          source: `/admin/event/${path.params.eventName}`,
+          destination: '/admin/event/[eventName]',
           query: { eventName: path.params.eventName },
-        };
+        });
       });
   
-      return pathMap;
+      return rewrites;
     },
   };
   
