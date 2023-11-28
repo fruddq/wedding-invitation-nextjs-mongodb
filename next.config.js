953ff47ff7befa00a39837cc9a0a618e1611456f
Frudd <giadi735@gmail.com>
2023-11-28 21:09:54 +0100

    Test next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        serverActions:true
+      serverActions: true,
     },
     basePath: '/github-pages',
     reactStrictMode: false,
     output: 'export',
     trailingSlash: true,
-
-}
-
-module.exports = nextConfig
+  
+    exportPathMap: async function () {
+      const paths = await generateStaticParams();
+      const pathMap = {};
+  
+      paths.forEach((path) => {
+        pathMap[`/admin/event/${path.params.eventName}`] = {
+          page: '/admin/event/[eventName]',
+          query: { eventName: path.params.eventName },
+        };
+      });
+  
+      return pathMap;
+    },
+  };
+  
+  module.exports = nextConfig;
