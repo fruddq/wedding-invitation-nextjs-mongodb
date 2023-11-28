0c9e8bec1329197b01542d24f6c4ce963bdf5bbd
nanidam <nani.dam@medieinstitutet.se>
2023-11-28 19:02:58 +0100

    fixed headwer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
-import HeaderSvgDesktop from "./headerSvgDesktop";
-import HeaderSvgMobile from "./headerSvgMobile";
 import "./styles/header.scss";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="header">
-      <HeaderSvgDesktop />
-      <HeaderSvgMobile />
+      <div className="header-mobile">
+        <Image src="/header.svg" alt="logo" width={280} height={70} />
+      </div>
+      <div className="header-desktop">
+        <Image src="/header.svg" alt="logo" width={580} height={200} />
+      </div>
     </header>
   );
 };
