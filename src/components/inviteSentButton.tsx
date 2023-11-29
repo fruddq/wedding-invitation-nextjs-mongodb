ed0cfa86636ec801251d07bfb5656f8a7daeca2f
Frudd <giadi735@gmail.com>
2023-11-29 20:30:45 +0100

    working on admin page

--- src/components/inviteSentButton.tsx
+++ src/components/inviteSentButton.tsx
@@ -1,12 +1,38 @@
-"use client";
+"use client"
 
-import { GuestInterface } from "@/app/invitation/[inviteCode]/page";
-import updateInviteSent from "@/utils/updateInviteSent";
-
+import { GuestInterface } from "@/app/invitation/[inviteCode]/page"
+import updateInviteSent from "@/utils/updateInviteSent"
+import { useState, useEffect } from "react"
+import PacmanLoader from "react-spinners/PacmanLoader"
+import "./styles/inviteSentButton.scss"
 export default function InviteSentButton({ guest }: { guest: GuestInterface }) {
-  return (
-    <button onClick={async () => updateInviteSent(guest)}>
-      {guest.inviteSent ? "Yes" : "No"}
+  const [inviteSentStatus, setInviteSentStatus] = useState("No")
+  const [loading, setloading] = useState(false)
+
+  useEffect(() => {
+    setInviteSentStatus(guest.inviteSent ? "Yes" : "No")
+  }, [guest.inviteSent, setInviteSentStatus])
+
+  const handleInviteSentClick = async () => {
+    setloading(true)
+    const result = await updateInviteSent(guest)
+    console.log(result)
+    setInviteSentStatus(result ? "Yes" : "No")
+    setloading(false)
+  }
+  return loading ? (
+    <div className="loader-container">
+      <PacmanLoader
+        color="orange"
+        loading={loading}
+        size={10}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  ) : (
+    <button className="invite-sent-button" onClick={handleInviteSentClick}>
+      {inviteSentStatus}
     </button>
-  );
+  )
 }
