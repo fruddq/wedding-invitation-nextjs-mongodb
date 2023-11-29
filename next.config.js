688110c7fe59d23d2cc5b95f84785945ee7cb73d
Frudd <giadi735@gmail.com>
2023-11-29 11:51:09 +0100

    Test cors

--- next.config.js
+++ next.config.js
@@ -3,14 +3,9 @@ const nextConfig = {
     experimental: {
       serverActions: true,
     },
-  
     basePath: '/github-pages',
-  
     reactStrictMode: true,
-
     trailingSlash: true,
-  
-
   };
   
   module.exports = nextConfig;
