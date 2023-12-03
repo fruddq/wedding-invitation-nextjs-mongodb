593ec4cb068ca4b258abc00ec8dc1c6049de707f
nanidam <nani.dam@medieinstitutet.se>
2023-12-04 00:57:59 +0100

    added head icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg+xml" href="/rings.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
