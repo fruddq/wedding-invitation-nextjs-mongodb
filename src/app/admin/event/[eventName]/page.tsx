609587832d83cec2cf0efe55518a9d28915f880a
nanidam <nani.dam@medieinstitutet.se>
2023-12-04 00:09:46 +0100

    :bug: fixed IEvent

--- src/app/admin/event/[eventName]/page.tsx
+++ src/app/admin/event/[eventName]/page.tsx
@@ -1,22 +1,33 @@
-import getEvent from "@/utils/getEvent"
-import "./style.scss"
-import EventComponent from "./eventComponent"
-import { Event } from "@/interface/interface"
+import getEvent from "@/utils/getEvent";
+import "./style.scss";
+import EventComponent from "./eventComponent";
+import { GuestInterface } from "@/interface/interface";
+
+interface IEvent {
+  guestList: GuestInterface[];
+  RSVPDate: Date | null;
+  id: string;
+  eventName: string | null;
+  eventDate: Date;
+  password: string;
+  inviteLink: string | null;
+  eventPlannerUserId: string;
+}
 
 export default async function Event({
   params: { eventName: eventNameParam },
 }: {
-  readonly params: { readonly eventName: string }
+  readonly params: { readonly eventName: string };
 }) {
   if (eventNameParam.includes(".")) {
-    return null
+    return null;
   }
 
-  const event = (await getEvent(eventNameParam)) as Event
+  const event = (await getEvent(eventNameParam)) as IEvent;
 
   if (!event) {
-    return null
+    return null;
   }
 
-  return <EventComponent event={event} />
+  return <EventComponent event={event} />;
 }
