25200068c8f19e2e54f9aa34380557a99b3c1227
Frudd <giadi735@gmail.com>
2023-12-03 19:34:35 +0100

    Fixed defaultvalue

--- src/app/admin/event/[eventName]/guestsComponent.tsx
+++ src/app/admin/event/[eventName]/guestsComponent.tsx
@@ -23,200 +23,20 @@ export const GuestsComponent = ({
 
     const email = formData.get("email")
     const attending = formData.get("attending")
+    const phoneNumber = formData.get("phone-number")
+    const hasResponded = formData.get("has-responded")
 
-    console.log(attending, email)
+    console.log(email, "email")
+    console.log(phoneNumber, "phoneNumber")
+    console.log(attending, "attending")
+    console.log(hasResponded, "hasResponded")
+
     setEditModeId(null)
   }
 
   const handleCancelClick = () => {
     setEditModeId(null)
   }
-
-  //   return (
-  //     <div className="guestlist-container">
-  //       <h2 className="guestlist-header">
-  //         Number of invited Guests: {guestList.length}
-  //       </h2>
-
-  //       {guestList.map((guest: GuestInterface) => (
-  //         <div key={guest.id} className="guest-container">
-  //           <form onSubmit={handleSaveClick}>
-  //             <div className="guest-info">
-  //               {editModeId === guest.id ? (
-  //                 <>
-  //                   <button type="submit">Save</button>
-  //                   <button onClick={handleCancelClick}>Cancel</button>
-  //                 </>
-  //               ) : (
-  //                 <>
-  //                   <button onClick={() => handleEditClick(guest.id)}>
-  //                     Edit
-  //                   </button>
-  //                 </>
-  //               )}
-
-  //               <hr />
-
-  //               <p className="guest-info-item guest-info-name">
-  //                 <span className="guest-info-label">First Name:</span>{" "}
-  //                 {guest.firstName}
-  //               </p>
-  //               <hr />
-  //               <p className="guest-info-item guest-info-lastname">
-  //                 <span className="guest-info-label">Last Name:</span>{" "}
-  //                 {guest.lastName}
-  //               </p>
-
-  //               <hr />
-
-  //               <p className="guest-info-item guest-info-email">
-  //                 <span className="guest-info-label">Email:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <input type="text" defaultValue={guest.email || "N/A"} />
-  //                 ) : (
-  //                   guest.email || "N/A"
-  //                 )}
-  //               </p>
-  //               <hr />
-
-  //               <p className="guest-info-item guest-info-phone">
-  //                 <span className="guest-info-label">Phone Number:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <input
-  //                     type="text"
-  //                     defaultValue={guest.phoneNumber || "N/A"}
-  //                   />
-  //                 ) : (
-  //                   guest.phoneNumber || "N/A"
-  //                 )}
-  //               </p>
-  //               <hr />
-
-  //               <p className="guest-info-item guest-info-diet">
-  //                 <span className="guest-info-label">Diet:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <input type="text" defaultValue={guest.diet || "N/A"} />
-  //                 ) : (
-  //                   guest.diet || "N/A"
-  //                 )}
-  //               </p>
-  //               <hr />
-
-  //               <p className="guest-info-item guest-info-comments">
-  //                 <span className="guest-info-label">Comments:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <input type="text" defaultValue={guest.comments || "N/A"} />
-  //                 ) : (
-  //                   guest.comments || "N/A"
-  //                 )}
-  //               </p>
-  //               <hr />
-  //               <p className="guest-attendance-status">
-  //                 <span className="attendance-label">Attending:</span>
-
-  //                 {editModeId === guest.id ? (
-  //                   <select>
-  //                     <option value="true" selected={guest.attending}>
-  //                       Yes
-  //                     </option>
-  //                     <option value="false" selected={!guest.attending}>
-  //                       No
-  //                     </option>
-  //                   </select>
-  //                 ) : guest.attending ? (
-  //                   "Yes"
-  //                 ) : (
-  //                   "No"
-  //                 )}
-  //               </p>
-  //               <hr />
-
-  //               <p className="guest-response-status">
-  //                 <span className="response-label">Has Responded:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <select>
-  //                     <option value="true" selected={guest.hasResponded}>
-  //                       Yes
-  //                     </option>
-  //                     <option value="false" selected={!guest.hasResponded}>
-  //                       No
-  //                     </option>
-  //                   </select>
-  //                 ) : guest.hasResponded ? (
-  //                   "Yes"
-  //                 ) : (
-  //                   "No"
-  //                 )}
-  //               </p>
-  //               <hr />
-
-  //               <p className="guest-invite-action">
-  //                 <span className="invite-label">Invite Sent:</span>
-  //                 {editModeId === guest.id ? (
-  //                   <select>
-  //                     <option value="true" selected={guest.inviteSent}>
-  //                       Yes
-  //                     </option>
-  //                     <option value="false" selected={!guest.inviteSent}>
-  //                       No
-  //                     </option>
-  //                   </select>
-  //                 ) : guest.inviteSent ? (
-  //                   "Yes"
-  //                 ) : (
-  //                   "No"
-  //                 )}
-  //               </p>
-  //             </div>
-
-  //             {guest.additionalGuests[0].firstName.length > 0 && (
-  //               <div className="additional-guests-container">
-  //                 <hr />
-  //                 <p className="additional-guests-header">Additional Guests:</p>
-  //                 {guest.additionalGuests.map((additionalGuest, index) => (
-  //                   <div key={index} className="additional-guest">
-  //                     <ul className="additional-guest-details">
-  //                       <li className="additional-guest-detail-item">
-  //                         <span className="detail-label">First Name:</span>{" "}
-  //                         {additionalGuest.firstName}
-  //                       </li>
-  //                       <li className="additional-guest-detail-item">
-  //                         <span className="detail-label">Last Name:</span>{" "}
-  //                         {additionalGuest.lastName}
-  //                       </li>
-  //                       <li className="additional-guest-detail-item">
-  //                         <span className="detail-label">Diet:</span>{" "}
-  //                         {editModeId === guest.id ? (
-  //                           <input
-  //                             type="text"
-  //                             defaultValue={additionalGuest.diet || "N/A"}
-  //                           />
-  //                         ) : (
-  //                           additionalGuest.diet || "N/A"
-  //                         )}
-  //                       </li>
-  //                       <li className="additional-guest-detail-item">
-  //                         <span className="detail-label">Comment:</span>{" "}
-  //                         {editModeId === guest.id ? (
-  //                           <input
-  //                             type="text"
-  //                             defaultValue={additionalGuest.comments || "N/A"}
-  //                           />
-  //                         ) : (
-  //                           additionalGuest.comments || "N/A"
-  //                         )}
-  //                       </li>
-  //                     </ul>
-  //                   </div>
-  //                 ))}
-  //               </div>
-  //             )}
-  //           </form>
-  //         </div>
-  //       ))}
-  //     </div>
-  //   )
-  // }
 
   return (
     <div className="guestlist-container">
@@ -248,7 +68,7 @@ export const GuestsComponent = ({
                 <hr />
 
                 <p className="guest-info-item guest-info-email">
-                  <span className="guest-info-label">Email:</span>
+                  <span className="guest-info-label">Email: </span>{" "}
                   <input
                     type="text"
                     name="email"
@@ -262,6 +82,7 @@ export const GuestsComponent = ({
                   <span className="guest-info-label">Phone Number:</span>
                   <input
                     type="text"
+                    name="phone-number"
                     defaultValue={guest.phoneNumber || "N/A"}
                   />
                 </p>
@@ -283,13 +104,12 @@ export const GuestsComponent = ({
                 <p className="guest-response-status">
                   <span className="response-label">Has Responded:</span>
 
-                  <select>
-                    <option value="true" selected={guest.hasResponded}>
-                      Yes
-                    </option>
-                    <option value="false" selected={!guest.hasResponded}>
-                      No
-                    </option>
+                  <select
+                    name="has-responded"
+                    defaultValue={guest.hasResponded.toString()}
+                  >
+                    <option value="true">Yes</option>
+                    <option value="false">No</option>
                   </select>
                 </p>
                 <hr />
