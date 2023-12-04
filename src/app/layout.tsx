a63f56283792cfda0940be79d01f93783d69ca20
Frudd <giadi735@gmail.com>
2023-12-04 18:04:16 +0100

    test viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import "./globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Wedding invitation",
   description: "Invitation to Frudd & Nanis wedding",
-};
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=0, maximum-scale=1"
+        />
         <link rel="icon" type="image/svg+xml" href="/rings.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
