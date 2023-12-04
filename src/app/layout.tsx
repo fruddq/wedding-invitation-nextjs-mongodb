efba913f4c458b7025867febec0a4aa7ea2343d4
Frudd <giadi735@gmail.com>
2023-12-04 18:09:45 +0100

    anotha test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=1000" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"
+        />
         <link rel="icon" type="image/svg+xml" href="/rings.png" />
       </head>
       <body className={inter.className}>{children}</body>
